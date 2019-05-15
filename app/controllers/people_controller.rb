9e78ec17b7b28e996f4e012c20586531447aa2ee
tallan groberg <tallan.taven@gmail.com>
2019-05-14 18:37:39 -0600

    crud actions fuctional

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -19,15 +19,28 @@ class PeopleController < ApplicationController
     else
       render :new
     end
+  end
 
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to people_path
+    else
+      render :edit
+    end
   end
   def edit
     @person = Person.find(params[:id])
   end
+  def destroy
+    Person.find(params[:id]).destroy
+    redirect_to people_path
+  end
+end
 
   private
 
 def person_params
   params.require(:person).permit(:first_name, :last_name, :age, :hair_color, :eye_color, :gender, :alive)
   end
-end
